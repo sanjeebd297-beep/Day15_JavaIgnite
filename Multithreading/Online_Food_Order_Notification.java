1c116d74a89b44ec7021c86f2a17bdc6a98a11c0
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-15 08:24:37 +0530

    Implement notification services for order confirmation

--- Multithreading/Online_Food_Order_Notification.java
+++ Multithreading/Online_Food_Order_Notification.java
@@ -92,3 +92,66 @@ Main Method
     Call placeOrder()
 
 */
+package JavaIgniteDay15;
+interface NotificationService 
+{
+    void sendMessage();
+}
+
+class EmailNotification implements NotificationService
+{
+
+    @Override
+    public void sendMessage() 
+    {
+        System.out.println("Email Notification Sent");
+    }
+}
+
+class SMSNotification implements NotificationService 
+{
+
+    @Override
+    public void sendMessage() 
+    {
+        System.out.println("SMS Notification Sent");
+    }
+}
+
+class OrderService 
+{
+
+    private NotificationService notificationService;
+
+    public OrderService(NotificationService notificationService)
+    {
+        this.notificationService = notificationService;
+    }
+
+    public void placeOrder() 
+    {
+        System.out.println("Order Confirmed");
+        notificationService.sendMessage();
+    }
+}
+
+
+public class Online_Food_Order {
+	
+	public static void main(String[] args) {
+    	// TODO Auto-generated method stub
+    	
+        NotificationService email = new EmailNotification();
+        OrderService order1 = new OrderService(email);
+
+        order1.placeOrder();
+
+        System.out.println();
+
+       
+        NotificationService sms = new SMSNotification();
+        OrderService order2 = new OrderService(sms);
+
+        order2.placeOrder();
+    }
+}
